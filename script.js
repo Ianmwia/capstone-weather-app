669bb528ae388d961203a151f4f441a81788752b
Mwia <iansnr10@gmail.com>
2025-10-18 10:16:18 +0300

    getting weather data from api from user input

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-const apiKey = 'dcc012d2c6a56b1396fcb5cf2fb684c3'
-const lat = 50
-const lon = 0
-const weatherApi = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
-
-fetch(weatherApi)
-.then(res => res.json())
-.then(data =>{
-    console.log('the api is working')
-    console.log(data)
-})
-.catch(error =>{
-    console.log(`error`, error)
-})
+let apiKey = '36fd92a69321111547844661f619effd'
 
 //dom
 const locationInput = document.getElementById('search')
 const form = document.getElementById('form')
+const currentLocation = document.getElementById('current-location')
 
 //event listener
-form.addEventListener('submit-btn', (e)=>{
+form.addEventListener('submit', (e)=>{
     e.preventDefault()
+
+    const city = locationInput.value
+    fetchAndDisplayWeather(city)
 })
+
+//geocode translate longitude and latitude
+//geocoding get city name
+async function getCoordinates(city) {
+    const cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(city)}&limit=1&appid=${apiKey}`
+    const result = await fetch(cityUrl)
+    const data = await result.json()
+    if (!data.length) throw 'City not Found'
+    return data [0]
+}
+
+async function getWeather(lat, lon) {
+    const cityCoord = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
+    const response = await fetch(cityCoord)
+    return response.json()
+    
+}
+
+async function fetchAndDisplayWeather(city) {
+    if (!city){
+        alert('enter a city name')
+        return
+    }
+    try {
+        const location = await getCoordinates(city)
+        const weather = await getWeather(location.lat, location.lon)
+
+        //append on html
+        currentLocation.innerHTML = `
+            <p>${location.name}</p>
+            <p>${new Date(weather.dt *1000).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}</p>
+            <p>${weather.main.temp.toFixed(1)}</p>
+            <p>${weather.weather[0].main}</p>
+        `
+    }catch(error){
+        console.error('error', error)
+    }
+    
+}
